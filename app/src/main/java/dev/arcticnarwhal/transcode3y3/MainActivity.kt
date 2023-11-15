01c8aa78f928c415e15a1aadfc2d3a3d161e4974
Narwhal <narwhal@arcticnarwhal.dev>
2023-11-15 23:17:01 +0100

    About section, helper text, design improvements
    The about section is reachable over the entry in the menu.
    It is implemented as a dialog and shows a text giving information
    about the project and liks to relevant pages. A know issue is
    that the links are not clickable (yet).
    
    There is a helper text below the input text field now, giving
    information about the current operation, encoding or decoding.
    The app now uses edge to edge layout, which until now gets only
    rid of the brighly colored task bar, but might be improved in the
    future to show scrolled elements behind the task bar.

--- app/src/main/java/dev/arcticnarwhal/transcode3y3/MainActivity.kt
+++ app/src/main/java/dev/arcticnarwhal/transcode3y3/MainActivity.kt
@@ -3,8 +3,17 @@ package dev.arcticnarwhal.transcode3y3
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.ViewGroup.MarginLayoutParams
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.core.widget.doAfterTextChanged
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import dev.arcticnarwhal.transcode3y3.databinding.ActivityMainBinding
 import java.util.function.IntUnaryOperator
@@ -15,12 +24,33 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var clipMan: ClipboardManager
+    private lateinit var dialogAbout: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, windowInsets ->
+            val insetsSystemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val insetsIme = windowInsets.getInsets(WindowInsetsCompat.Type.ime())
+            // Apply the insetsSystemBars as a margin to the view. This solution sets
+            // only the bottom, left, and right dimensions, but you can apply whichever
+            // insetsSystemBars are appropriate to your layout. You can also update the view padding
+            // if that's more appropriate.
+
+            view.updateLayoutParams<MarginLayoutParams> {
+                leftMargin = insetsSystemBars.left
+                bottomMargin = if (insetsIme.bottom != 0) insetsIme.bottom else insetsSystemBars.bottom
+                rightMargin = insetsSystemBars.right
+            }
+
+            windowInsets
+        }
+
+        setSupportActionBar(binding.toolbar)
 
         clipMan = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
 
@@ -34,21 +64,47 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.input.doAfterTextChanged {
-            val text = binding.input.text.toString()
-            if (text.isEmpty()) {
-                binding.inputLayout.helperText = getString(R.string.helper_empty)
-            } else {
-                binding.output.setText(transcode3y3(binding.input.text.toString()))
-            }
+            binding.output.setText(transcode3y3(binding.input.text.toString()))
         }
+
+        val builder = MaterialAlertDialogBuilder(this)
+        builder
+            .setMessage(R.string.dialog_about)
+            .setTitle(R.string.dialog_about_title)
+            .setPositiveButton(getString(R.string.btn_close)) { _, _ -> }
+
+        dialogAbout = builder.create()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_about -> onAboutClicked()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    private fun onAboutClicked(): Boolean {
+        dialogAbout.show()
+        return true
     }
 
     //Originally implemented by Twilight Sparkle (@yourcompanionAI). See https://synthetic.garden/3y3.htm
-
     private var from3y3: IntUnaryOperator = IntUnaryOperator { cp -> if (cp in 0xe0001..0xe007e) cp - 0xe0000 else cp }
     private var to3y3: IntUnaryOperator = IntUnaryOperator { cp -> if (cp in 0x01..0x7e) cp + 0xe0000 else cp }
 
     private fun transcode3y3(text: String): String {
+        if (text.isEmpty()) {
+            binding.inputLayout.helperText = getString(R.string.helper_empty)
+            return ""
+        }
         for (cp in text.codePoints()) {
             if (cp in 0xe0001..0xe007e) {
                 binding.inputLayout.helperText = getString(R.string.helper_decoding)
